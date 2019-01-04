d8392a94d431e608cee2b41d6d6b25fbb5269409
Peter Kronström <peter.kronstrom@reaktor.com>
2019-01-04 12:48:46 +0200

    Add new battery services

--- src/services/servicemapping.js
+++ src/services/servicemapping.js
@@ -3,16 +3,45 @@ const BATTERY =  {
     "service": "com.victronenergy.battery",
     "paths": [
         {
-            "label": "Voltage (V)",
+            "label": "Voltage 1 (V)",
             "path": "/Dc/0/Voltage"
+        },
+        {
+            "label": "Voltage 2 (V)",
+            "path": "/Dc/1/Voltage"
         },
         {
             "label": "Current (A)",
             "path": "/Dc/0/Current"
         },
         {
-            "label": "Power (W)",
-            "path": "/Dc/0/Power"
+            "label": "Temperature (°C)",
+            "path": "/Dc/0/Temperature"
+        },
+        {
+            "label": "Consumed Charge (Ah)",
+            "path": "/ConsumedAmphours"
+        },
+        {
+            "label": "State of Charge (%)",
+            "path": "/Soc"
+        },
+        {
+            "label": "Time to Go (s)",
+            "path": "/TimeToGo"
+        },
+        {
+            "label": "Battery Low Threshold (V)",
+            "path": "/Info/BatteryLowVoltage"
+        },
+        {
+            "label": "Relay State (on/off)",
+            "path": "/Relay/0/State"
+        },
+        ,
+        {
+            "label": "Ext. Relay State - Lynx (on/off)",
+            "path": "/Io/ExternalRelay"
         }
     ]
 }
