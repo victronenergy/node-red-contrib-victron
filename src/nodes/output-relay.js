2d67f81ef740c44fb4dedd10fc44d39310f91751
Peter Kronström <peter.kronstrom@reaktor.com>
2019-01-17 13:56:22 +0200

    Use cached relay state as initial toggle value
    Fetch the initial relay state from the cache when the node is set on toggle
    instead of using a hardcoded initial value.

--- src/nodes/output-relay.js
+++ src/nodes/output-relay.js
@@ -12,26 +12,24 @@ module.exports = function(RED) {
         this.config = RED.nodes.getNode("victron-client-id")
         this.client = this.config.client
 
-        let toggleState = 0
-
-        let stateToMessage = state => {
+        let stateToMessage = (state, previousState) => {
             switch(state) {
                 case 'on':
                     return 1
                 case 'off':
                     return 0
                 case 'toggle':
-                    toggleState = 1 - toggleState
-                    return toggleState
+                    return 1 - parseInt(previousState)
             }
         };
 
         this.on("input", function(msg) {
-            let path = this.service.paths[0].path
-            let service = this.service.service
+            const path = this.service.paths[0].path
+            const service = this.service.service
+            const previousState = this.client.system.cache[service][path]
 
             if (!this.service.disabled)
-                this.client.publish(service, path, stateToMessage(this.state))
+                this.client.publish(service, path, stateToMessage(this.state, previousState))
             else
                 this.warn(mapping.RELAY_MODE_WARNING('another'))
         });
