3d7a1563c1841cf5ef60b3b1a5893316f16f82ba
Dirk-Jan Faber <d.faber@gmail.com>
2024-12-09 10:35:51 +0100

    Simplify vscode debugging (developers)
    Make debugging from vscode easier, using a specific Node-RED
    settings file, which is stored in the .vscode directory.

---
+++ .vscode/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+    uiPort: 1880,
+    flowFile: 'flows.json',
+    userDir: process.env.HOME + '/.node-red',
+    functionGlobalContext: {},
+    debugMaxLength: 1000,
+    nodesDir: [process.env.HOME + '/git/node-red-contrib-victron/src/nodes'],
+    verbose: true,
+    logging: {
+        console: {
+            level: "debug",
+            metrics: false,
+            audit: false
+        }
+    }
+};
