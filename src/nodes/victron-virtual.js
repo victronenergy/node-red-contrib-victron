0af2b08a228d4561923dedaf54c6cb6f329df25d
Dirk-Jan Faber <dfaber@victronenergy.com>
2025-06-17 13:05:34 +0200

    Add virtual generator support

--- src/nodes/victron-virtual.js
+++ src/nodes/victron-virtual.js
@@ -18,6 +18,55 @@ process.on('unhandledRejection', (reason, promise) => {
   console.error('=== END DEBUG ===')
 })
 
+const commonGeneratorProperties = {
+  'AutoStart': { type: 'i', format: (v) => v != null ? v : '', value: 1, persist: true },
+  'Start': { type: 'i', format: (v) => v != null ? v : '', value: 0, persist: true },
+  'RemoteStartModeEnabled': { type: 'i', format: (v) => v != null ? v : '', value: 1, persist: true },
+  'EnableRemoteStartMode': { type: 'i', format: (v) => v != null ? v : '', value: 0, persist: true },
+  'Engine/CoolantTemperature': { type: 'd', format: (v) => v != null ? v.toFixed(1) + 'C' : '' },
+  'Engine/ExhaustTemperature': { type: 'd', format: (v) => v != null ? v.toFixed(1) + 'C' : '' },
+  'Engine/OilTemperature': { type: 'd', format: (v) => v != null ? v.toFixed(1) + 'C' : '' },
+  'Engine/OilPressure': { type: 'd', format: (v) => v != null ? v.toFixed(1) + 'bar' : '' },
+  'Engine/WindingTemperature': { type: 'd', format: (v) => v != null ? v.toFixed(1) + 'C' : '' },
+  'Engine/Starts': { type: 'i', format: (v) => v != null ? v : '', value: 0, persist: 60 },
+  'Engine/Load': { type: 'd', format: (v) => v != null ? v.toFixed(1) + '%' : '' },
+  'Engine/Speed': { type: 'i', format: (v) => v != null ? v + 'RPM' : '' },
+  'Engine/OperatingHours': { type: 'd', format: (v) => v != null ? v.toFixed(1) + 'h' : '', persist: 300 },
+  'Alarms/HighTemperature': { type: 'i', format: (v) => v != null ? v : '', value: 0 },
+  'Alarms/LowOilPressure': { type: 'i', format: (v) => v != null ? v : '', value: 0 },
+  'Alarms/LowCoolantLevel': { type: 'i', format: (v) => v != null ? v : '', value: 0 },
+  'Alarms/LowOilLevel': { type: 'i', format: (v) => v != null ? v : '', value: 0 },
+  'Alarms/LowFuelLevel': { type: 'i', format: (v) => v != null ? v : '', value: 0 },
+  'Alarms/LowStarterVoltage': { type: 'i', format: (v) => v != null ? v : '', value: 0 },
+  'Alarms/HighStarterVoltage': { type: 'i', format: (v) => v != null ? v : '', value: 0 },
+  'Alarms/EmergencyStop': { type: 'i', format: (v) => v != null ? v : '', value: 0 },
+  'Alarms/ServicesNeeded': { type: 'i', format: (v) => v != null ? v : '', value: 0 },
+  'Alarms/GenericAlarm': { type: 'i', format: (v) => v != null ? v : '', value: 0 },
+  StatusCode: {
+    type: 'i',
+    format: (v) => ({
+      0: 'Standby',
+      1: 'Startup 1',
+      2: 'Startup 2',
+      3: 'Startup 3',
+      4: 'Startup 4',
+      5: 'Startup 5',
+      6: 'Startup 6',
+      7: 'Startup 7',
+      8: 'Running',
+      9: 'Cooldown',
+      10: 'Stopping',
+      11: 'Error'
+    }[v] || 'unknown'),
+    value: 0
+  },
+  ErrorCode: { type: 'i', format: (v) => v != null ? v : '', value: 0 },
+  'StarterVoltage': { type: 'd', format: (v) => v != null ? v.toFixed(2) + 'V' : '', persist: true },
+  'FirmwareVersion': { type: 's', format: (v) => v != null ? v : '', persist: true },
+  'Model': { type: 's', format: (v) => v != null ? v : '', persist: true },
+  Connected: { type: 'i', format: (v) => v != null ? v : '', value: 1 }
+}
+
 const properties = {
   battery: {
     Capacity: { type: 'd', format: (v) => v != null ? v.toFixed(0) + 'Ah' : '', persist: true },
@@ -71,6 +120,28 @@ const properties = {
     Humidity: { type: 'd', format: (v) => v != null ? v.toFixed(1) + '%' : '', persist: 60 },
     BatteryVoltage: { type: 'd', value: 3.3, format: (v) => v != null ? v.toFixed(2) + 'V' : '', persist: 300 },
     Status: { type: 'i', persist: true /* persist on every state change */ }
+  },
+  genset: {
+    ...commonGeneratorProperties,
+    'Ac/Power': { type: 'd', format: (v) => v != null ? v.toFixed(2) + 'W' : '' },
+    'Ac/Energy/Forward': { type: 'd', format: (v) => v != null ? v.toFixed(2) + 'kWh' : '' },
+    'Ac/Frequency': { type: 'd', format: (v) => v != null ? v.toFixed(1) + 'Hz' : '' },
+    NrOfPhases: { type: 'i', format: (v) => v != null ? v : '', value: 1 }
+  },
+  dcgenset: {
+    ...commonGeneratorProperties,
+    'Dc/0/Current': { type: 'd', format: (v) => v != null ? v.toFixed(2) + 'A' : '' },
+    'Dc/0/Power': { type: 'd', format: (v) => v != null ? v.toFixed(2) + 'W' : '' },
+    'Dc/0/Voltage': { type: 'd', format: (v) => v != null ? v.toFixed(2) + 'V' : '' },
+    'History/EnergyOut': { type: 'd', format: (v) => v != null ? v.toFixed(2) + 'kWh' : '' },
+    State: {
+      type: 'i',
+      format: (v) => ({
+        0: 'Stopped',
+        1: 'Running'
+      }[v] || 'unknown'),
+      value: 0
+    },
   },
   grid: {
     'Ac/Energy/Forward': { type: 'd', format: (v) => v != null ? v.toFixed(2) + 'kWh' : '', value: 0 },
@@ -207,7 +278,8 @@ const properties = {
 }
 
 function getIfaceDesc (dev) {
-  if (!properties[dev]) {
+  const actualDev = dev === 'generator' ? 'genset' : dev
+  if (!properties[actualDev]) {
     return {}
   }
 
@@ -229,7 +301,8 @@ function getIfaceDesc (dev) {
 }
 
 function getIface (dev) {
-  if (!properties[dev]) {
+  const actualDev = dev === 'generator' ? 'genset' : dev
+  if (!properties[actualDev]) {
     return {
       emit: function () {
       }
@@ -302,12 +375,11 @@ module.exports = function (RED) {
         return
       }
 
-      let serviceName = `com.victronenergy.${config.device}.virtual_${self.id}`
-      // For relays, we only add services, setting the serviceName to this (will result in 0x3 code)
-      if (config.device === 'relay') {
-        serviceName = 'com.victronenergy.settings'
-      }
+      const actualDeviceType = config.device === 'generator'
+        ? (config.generator_type === 'dc' ? 'dcgenset' : 'genset')
+        : config.device
 
+      let serviceName = `com.victronenergy.${actualDeviceType}.virtual_${self.id}`
       const interfaceName = serviceName
       const objectPath = `/${serviceName.replace(/\./g, '/')}`
 
@@ -403,13 +475,13 @@ module.exports = function (RED) {
           name: interfaceName,
           methods: {
           },
-          properties: getIfaceDesc(config.device),
+          properties: getIfaceDesc(actualDeviceType),
           signals: {
           }
         }
 
         // Then we need to create the interface implementation (with actual functions)
-        const iface = getIface(config.device)
+        const iface = getIface(actualDeviceType)
 
         iface.Status = 0
         iface.Serial = node.id || '-'
@@ -439,7 +511,82 @@ module.exports = function (RED) {
             text = `Virtual ${properties.battery.Capacity.format(iface.Capacity)} battery`
             break
           }
-          case 'grid': {
+
+        case 'generator': {
+          const generatorType = config.generator_type === 'dc' ? 'dcgenset' : 'genset'
+          const nrOfPhases = Number(config.generator_nrofphases ?? 1)
+
+          if (generatorType === 'genset') {
+            const properties = [
+              { name: 'Current', unit: 'A' },
+              { name: 'Power', unit: 'W' },
+              { name: 'Voltage', unit: 'V' }
+            ]
+
+            for (let i = 1; i <= nrOfPhases; i++) {
+              const phase = `L${i}`
+              properties.forEach(({ name, unit }) => {
+                const key = `Ac/${phase}/${name}`
+                ifaceDesc.properties[key] = {
+                  type: 'd',
+                  format: (v) => v != null ? v.toFixed(2) + unit : ''
+                }
+                iface[key] = 0
+              })
+            }
+
+            iface.NrOfPhases = nrOfPhases
+          }
+
+          if (!config.include_engine_hours) {
+            delete ifaceDesc.properties['Engine/OperatingHours']
+            delete iface['Engine/OperatingHours']
+          }
+          if (!config.include_starter_voltage) {
+            delete ifaceDesc.properties.StarterVoltage
+            delete iface.StarterVoltage
+            delete ifaceDesc.properties['Alarms/LowStarterVoltage']
+            delete iface['Alarms/LowStarterVoltage']
+            delete ifaceDesc.properties['Alarms/HighStarterVoltage']
+            delete iface['Alarms/HighStarterVoltage']
+          }
+          if (generatorType === 'dcgenset' && !config.include_history_energy) {
+            delete ifaceDesc.properties['History/EnergyOut']
+            delete iface['History/EnergyOut']
+          }
+
+          if (config.default_values) {
+            iface['Engine/Load'] = 0
+            iface['Engine/Speed'] = 0
+            iface.StatusCode = 0
+            iface.State = 0
+
+            if (generatorType === 'dcgenset') {
+              iface['Dc/0/Current'] = 0
+              iface['Dc/0/Voltage'] = 48
+              iface['Dc/0/Power'] = 0
+              iface['Dc/0/Temperature'] = 25
+              if (config.include_history_energy) {
+                iface['History/EnergyOut'] = 0
+              }
+            } else {
+              iface['Ac/Power'] = 0
+              iface['Ac/Energy/Forward'] = 0
+            }
+
+            if (config.include_engine_hours) {
+              iface['Engine/OperatingHours'] = 0
+            }
+            if (config.include_starter_voltage) {
+              iface.StarterVoltage = 12
+            }
+          }
+
+          text = `Virtual ${generatorType === 'dcgenset' ? 'DC' : `${nrOfPhases}-phase AC`} generator`
+          break
+        }
+
+        case 'grid': {
             iface.NrOfPhases = Number(config.grid_nrofphases ?? 1)
             const properties = [
               { name: 'Current', unit: 'A' },
