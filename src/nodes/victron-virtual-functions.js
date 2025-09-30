4bb886998223ffd7c711128714ff336c24204689
Chris Oloff <chris@uber5.com>
2025-09-30 17:02:28 +0200

    Add common fields to basic slider
    Fixes bug I introduced when resolving merge conflict from master

--- src/nodes/victron-virtual-functions.js
+++ src/nodes/victron-virtual-functions.js
@@ -39,6 +39,7 @@ export const SWITCH_TYPE_CONFIGS = {
   // 6: {
   //   label: 'Dropdown',
   //   fields: [
+  //     ...COMMON_SWITCH_FIELDS,
   //     {
   //       id: 'count',
   //       type: 'number',
@@ -53,6 +54,7 @@ export const SWITCH_TYPE_CONFIGS = {
   7: {
     label: 'Basic slider',
     fields: [
+      ...COMMON_SWITCH_FIELDS,
       { id: 'min', type: 'number', placeholder: 'Min value', title: 'Slider minimum', style: 'width:80px;' },
       { id: 'max', type: 'number', placeholder: 'Max value', title: 'Slider maximum', style: 'width:80px;' },
       { id: 'step', type: 'number', placeholder: 'Step size', title: 'Step size', style: 'width:80px;' },
