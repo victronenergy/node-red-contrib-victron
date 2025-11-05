7eedab7f0f1c2c8f5f57ce20411080eca75996aa
Dirk-Jan Faber <dfaber@victronenergy.com>
2025-11-05 22:02:24 +0100

    fix: prevent global namespace pollution with window.__victron
    Closes #315

--- resources/victron-virtual-functions.js
+++ resources/victron-virtual-functions.js
@@ -614,17 +614,19 @@
 
   // src/nodes/victron-virtual-browser.js
 
-  window.checkGeneratorType = checkGeneratorType;
-  window.SWITCH_TYPE_CONFIGS = SWITCH_TYPE_CONFIGS;
-  window.SWITCH_OUTPUT_CONFIG = victronVirtualConstantsExports.SWITCH_OUTPUT_CONFIG;
-  window.SWITCH_SECOND_OUTPUT_LABEL = victronVirtualConstantsExports.SWITCH_SECOND_OUTPUT_LABEL;
-  window.SWITCH_THIRD_OUTPUT_LABEL = victronVirtualConstantsExports.SWITCH_THIRD_OUTPUT_LABEL;
-  window.renderSwitchConfigRow = renderSwitchConfigRow;
-  window.updateSwitchConfig = updateSwitchConfig;
-  window.checkSelectedVirtualDevice = checkSelectedVirtualDevice;
-  window.validateSwitchConfig = validateSwitchConfig;
-  window.updateBatteryVoltageVisibility = updateBatteryVoltageVisibility;
-  window.calculateOutputs = calculateOutputs;
-  window.updateOutputs = updateOutputs;
+  window.__victron = {
+    checkGeneratorType,
+    SWITCH_TYPE_CONFIGS,
+    SWITCH_OUTPUT_CONFIG: victronVirtualConstantsExports.SWITCH_OUTPUT_CONFIG,
+    SWITCH_SECOND_OUTPUT_LABEL: victronVirtualConstantsExports.SWITCH_SECOND_OUTPUT_LABEL,
+    SWITCH_THIRD_OUTPUT_LABEL: victronVirtualConstantsExports.SWITCH_THIRD_OUTPUT_LABEL,
+    renderSwitchConfigRow,
+    updateSwitchConfig,
+    checkSelectedVirtualDevice,
+    validateSwitchConfig,
+    updateBatteryVoltageVisibility,
+    calculateOutputs,
+    updateOutputs
+  };
 
 })();
--- src/nodes/victron-virtual-browser.js
+++ src/nodes/victron-virtual-browser.js
@@ -14,15 +14,17 @@ import {
   updateOutputs
 } from './victron-virtual-functions.js'
 
-window.checkGeneratorType = checkGeneratorType
-window.SWITCH_TYPE_CONFIGS = SWITCH_TYPE_CONFIGS
-window.SWITCH_OUTPUT_CONFIG = SWITCH_OUTPUT_CONFIG
-window.SWITCH_SECOND_OUTPUT_LABEL = SWITCH_SECOND_OUTPUT_LABEL
-window.SWITCH_THIRD_OUTPUT_LABEL = SWITCH_THIRD_OUTPUT_LABEL
-window.renderSwitchConfigRow = renderSwitchConfigRow
-window.updateSwitchConfig = updateSwitchConfig
-window.checkSelectedVirtualDevice = checkSelectedVirtualDevice
-window.validateSwitchConfig = validateSwitchConfig
-window.updateBatteryVoltageVisibility = updateBatteryVoltageVisibility
-window.calculateOutputs = calculateOutputs
-window.updateOutputs = updateOutputs
+window.__victron = {
+  checkGeneratorType,
+  SWITCH_TYPE_CONFIGS,
+  SWITCH_OUTPUT_CONFIG,
+  SWITCH_SECOND_OUTPUT_LABEL,
+  SWITCH_THIRD_OUTPUT_LABEL,
+  renderSwitchConfigRow,
+  updateSwitchConfig,
+  checkSelectedVirtualDevice,
+  validateSwitchConfig,
+  updateBatteryVoltageVisibility,
+  calculateOutputs,
+  updateOutputs
+}
