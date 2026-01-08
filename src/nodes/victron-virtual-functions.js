f1370b89243e2614d30d36f9a661327c5fc211b3
Chris Oloff <chris@uber5.com>
2026-01-08 12:39:24 +0200

    virtual devices views: fix increasing whitespace when switching device type, fix battery note width

--- src/nodes/victron-virtual-functions.js
+++ src/nodes/victron-virtual-functions.js
@@ -210,7 +210,7 @@ export function renderSwitchConfigRow (context) {
   function renderTypeConfig () {
     $('#switch-1-config-row').remove()
     $('#switch-1-pairs-row').remove()
-    $('#switch-1-doc-row').remove()
+    $('#switch-docs-container').empty()
 
     const type = $('#node-input-switch_1_type').val()
     const cfg = SWITCH_TYPE_CONFIGS[type]
