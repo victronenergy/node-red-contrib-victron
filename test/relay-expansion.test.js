victronenergy/node-red-contrib-victron

commit 829026da6cdcfff54160fa37500024490be77130

feat: add tooltips and community link to input/output nodes
- Extract tooltip system from virtual device switches to shared utility
- Add info icon tooltips to form fields
- Add community link button at bottom of node edit dialogs
- Improve form field alignment
- Enhance "Currently stored selection" display with info icon
- Add communityTag to services.json for generating the community links (not used yet)
- Use .svg instead of .png files

src/services/victron-system.js
@@ -20,10 +20,16 @@ class SystemConfiguration {
      * Filter the dbus cache for available device services.
      */
   getNodeServices (nodeName) {
-    const servicesWhitelist = _.get(utils.SERVICES, [nodeName.replace(/^(input-|output-)/g, '')])
+    const serviceType = nodeName.replace(/^(input-|output-)/g, '')
+    const servicesWhitelist = _.get(utils.SERVICES, [serviceType])
+    const communityTag = _.get(utils.SERVICES, [serviceType, 'communityTag'])
     const isOutput = nodeName.startsWith('output')
 
-    return Object.entries(servicesWhitelist || {})
+    // Filter out meta fields like communityTag and help
+    const serviceEntries = Object.entries(servicesWhitelist || {})
+      .filter(([key]) => key !== 'communityTag' && key !== 'help')
+
+    const services = serviceEntries
       .reduce((acc, [dbusService, servicePaths]) => {
         const cachedService = _.pickBy(this.cache, (val, key) => key.startsWith(`com.victronenergy.${dbusService}`))
 
@@ -63,11 +69,16 @@ class SystemConfiguration {
 
           const deviceInstance = cachedPaths['/DeviceInstance'] || ''
           if (expandedPaths.length) {
-            acc.push(utils.TEMPLATE(dbusInterface, name, deviceInstance, expandedPaths))
+            acc.push(utils.TEMPLATE(dbusInterface, name, deviceInstance, expandedPaths, communityTag))
           }
         })
         return acc
       }, [])
+
+    return {
+      communityTag: communityTag || null,
+      services
+    }
   }
 
   /**

resources/docs/basic_slider.svg
@@ -1,0 +1,12 @@
+<svg width="336" height="40" viewBox="0 0 336 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="1" y="1" width="334" height="38" rx="5" fill="#BBCEE0" stroke="#387DC5" stroke-width="2"/>
+<g clip-path="url(#clip0_15790_236211)">
+<rect x="255" width="40" height="336" transform="rotate(90 255 0)" fill="#387DC5"/>
+<rect x="247" y="6" width="4" height="28" rx="2" fill="white"/>
+</g>
+<defs>
+<clipPath id="clip0_15790_236211">
+<rect width="336" height="40" rx="6" fill="white"/>
+</clipPath>
+</defs>
+</svg>

resources/docs/community-link.svg
@@ -1,0 +1,7 @@
+<svg width="255" height="40" viewBox="0 0 255 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="1" y="1" width="253" height="38" rx="7" fill="#005FBE"/>
+<rect x="1" y="1" width="253" height="38" rx="7" stroke="#387DC5" stroke-width="2"/>
+<path d="M44.4902 23.0264H46.5527C46.4453 24.0146 46.1624 24.8991 45.7041 25.6797C45.2458 26.4603 44.5977 27.0798 43.7598 27.5381C42.9219 27.9893 41.8763 28.2148 40.623 28.2148C39.7064 28.2148 38.8721 28.043 38.1201 27.6992C37.3753 27.3555 36.7344 26.8685 36.1973 26.2383C35.6602 25.6009 35.2448 24.8382 34.9512 23.9502C34.6647 23.055 34.5215 22.0596 34.5215 20.9639V19.4062C34.5215 18.3105 34.6647 17.3187 34.9512 16.4307C35.2448 15.5355 35.6637 14.7692 36.208 14.1318C36.7594 13.4945 37.4219 13.0039 38.1953 12.6602C38.9688 12.3164 39.8389 12.1445 40.8057 12.1445C41.9873 12.1445 42.9863 12.3665 43.8027 12.8105C44.6191 13.2546 45.2529 13.8704 45.7041 14.6582C46.1624 15.4388 46.4453 16.3447 46.5527 17.376H44.4902C44.39 16.6455 44.2038 16.0189 43.9316 15.4961C43.6595 14.9661 43.2728 14.5579 42.7715 14.2715C42.2702 13.985 41.6149 13.8418 40.8057 13.8418C40.111 13.8418 39.4987 13.9743 38.9688 14.2393C38.446 14.5042 38.0055 14.8802 37.6475 15.3672C37.2965 15.8542 37.0316 16.4378 36.8525 17.1182C36.6735 17.7985 36.584 18.554 36.584 19.3848V20.9639C36.584 21.7301 36.6628 22.4499 36.8203 23.123C36.985 23.7962 37.2321 24.387 37.5615 24.8955C37.891 25.404 38.3099 25.805 38.8184 26.0986C39.3268 26.3851 39.9284 26.5283 40.623 26.5283C41.5039 26.5283 42.2057 26.3887 42.7285 26.1094C43.2513 25.8301 43.6452 25.429 43.9102 24.9062C44.1823 24.3835 44.3757 23.7568 44.4902 23.0264ZM48.5508 22.3174V22.0703C48.5508 21.2324 48.6725 20.4554 48.916 19.7393C49.1595 19.016 49.5104 18.3893 49.9688 17.8594C50.4271 17.3223 50.9821 16.9069 51.6338 16.6133C52.2855 16.3125 53.016 16.1621 53.8252 16.1621C54.6416 16.1621 55.3757 16.3125 56.0273 16.6133C56.6862 16.9069 57.2448 17.3223 57.7031 17.8594C58.1686 18.3893 58.5231 19.016 58.7666 19.7393C59.0101 20.4554 59.1318 21.2324 59.1318 22.0703V22.3174C59.1318 23.1553 59.0101 23.9323 58.7666 24.6484C58.5231 25.3646 58.1686 25.9912 57.7031 26.5283C57.2448 27.0583 56.6898 27.4736 56.0381 27.7744C55.3936 28.068 54.6631 28.2148 53.8467 28.2148C53.0303 28.2148 52.2962 28.068 51.6445 27.7744C50.9928 27.4736 50.4342 27.0583 49.9688 26.5283C49.5104 25.9912 49.1595 25.3646 48.916 24.6484C48.6725 23.9323 48.5508 23.1553 48.5508 22.3174ZM50.5381 22.0703V22.3174C50.5381 22.8975 50.6061 23.4453 50.7422 23.9609C50.8783 24.4694 51.0824 24.9206 51.3545 25.3145C51.6338 25.7083 51.9811 26.0199 52.3965 26.249C52.8118 26.471 53.2952 26.582 53.8467 26.582C54.391 26.582 54.8672 26.471 55.2754 26.249C55.6908 26.0199 56.0345 25.7083 56.3066 25.3145C56.5788 24.9206 56.7829 24.4694 56.9189 23.9609C57.0622 23.4453 57.1338 22.8975 57.1338 22.3174V22.0703C57.1338 21.4974 57.0622 20.9567 56.9189 20.4482C56.7829 19.9326 56.5752 19.4779 56.2959 19.084C56.0238 18.6829 55.68 18.3678 55.2646 18.1387C54.8564 17.9095 54.3766 17.7949 53.8252 17.7949C53.2809 17.7949 52.8011 17.9095 52.3857 18.1387C51.9775 18.3678 51.6338 18.6829 51.3545 19.084C51.0824 19.4779 50.8783 19.9326 50.7422 20.4482C50.6061 20.9567 50.5381 21.4974 50.5381 22.0703ZM63.6006 18.6865V28H61.6025V16.377H63.4932L63.6006 18.6865ZM63.1924 21.748L62.2686 21.7158C62.2757 20.9209 62.3796 20.1868 62.5801 19.5137C62.7806 18.8333 63.0778 18.2425 63.4717 17.7412C63.8656 17.2399 64.3561 16.8532 64.9434 16.5811C65.5306 16.3018 66.2109 16.1621 66.9844 16.1621C67.5286 16.1621 68.0299 16.2409 68.4883 16.3984C68.9466 16.5488 69.3441 16.7887 69.6807 17.1182C70.0173 17.4476 70.2786 17.8701 70.4648 18.3857C70.651 18.9014 70.7441 19.5244 70.7441 20.2549V28H68.7568V20.3516C68.7568 19.7428 68.653 19.2559 68.4453 18.8906C68.2448 18.5254 67.9583 18.2604 67.5859 18.0957C67.2135 17.9238 66.7767 17.8379 66.2754 17.8379C65.6882 17.8379 65.1976 17.9417 64.8037 18.1494C64.4098 18.3571 64.0947 18.6436 63.8584 19.0088C63.6221 19.374 63.4502 19.793 63.3428 20.2656C63.2425 20.7311 63.1924 21.2253 63.1924 21.748ZM70.7227 20.6523L69.3906 21.0605C69.3978 20.4232 69.5016 19.8109 69.7021 19.2236C69.9098 18.6364 70.207 18.1136 70.5938 17.6553C70.9876 17.1969 71.471 16.8353 72.0439 16.5703C72.6169 16.2982 73.2721 16.1621 74.0098 16.1621C74.6328 16.1621 75.1842 16.2445 75.6641 16.4092C76.151 16.5739 76.5592 16.8281 76.8887 17.1719C77.2253 17.5085 77.4795 17.9417 77.6514 18.4717C77.8232 19.0016 77.9092 19.6318 77.9092 20.3623V28H75.9111V20.3408C75.9111 19.6891 75.8073 19.1842 75.5996 18.8262C75.3991 18.4609 75.1126 18.2067 74.7402 18.0635C74.375 17.9131 73.9382 17.8379 73.4297 17.8379C72.9928 17.8379 72.6061 17.9131 72.2695 18.0635C71.9329 18.2139 71.6501 18.4215 71.4209 18.6865C71.1917 18.9443 71.0163 19.2415 70.8945 19.5781C70.7799 19.9147 70.7227 20.2728 70.7227 20.6523ZM82.8936 18.6865V28H80.8955V16.377H82.7861L82.8936 18.6865ZM82.4854 21.748L81.5615 21.7158C81.5687 20.9209 81.6725 20.1868 81.873 19.5137C82.0736 18.8333 82.3708 18.2425 82.7646 17.7412C83.1585 17.2399 83.6491 16.8532 84.2363 16.5811C84.8236 16.3018 85.5039 16.1621 86.2773 16.1621C86.8216 16.1621 87.3229 16.2409 87.7812 16.3984C88.2396 16.5488 88.637 16.7887 88.9736 17.1182C89.3102 17.4476 89.5716 17.8701 89.7578 18.3857C89.944 18.9014 90.0371 19.5244 90.0371 20.2549V28H88.0498V20.3516C88.0498 19.7428 87.946 19.2559 87.7383 18.8906C87.5378 18.5254 87.2513 18.2604 86.8789 18.0957C86.5065 17.9238 86.0697 17.8379 85.5684 17.8379C84.9811 17.8379 84.4906 17.9417 84.0967 18.1494C83.7028 18.3571 83.3877 18.6436 83.1514 19.0088C82.915 19.374 82.7432 19.793 82.6357 20.2656C82.5355 20.7311 82.4854 21.2253 82.4854 21.748ZM90.0156 20.6523L88.6836 21.0605C88.6908 20.4232 88.7946 19.8109 88.9951 19.2236C89.2028 18.6364 89.5 18.1136 89.8867 17.6553C90.2806 17.1969 90.764 16.8353 91.3369 16.5703C91.9098 16.2982 92.5651 16.1621 93.3027 16.1621C93.9258 16.1621 94.4772 16.2445 94.957 16.4092C95.444 16.5739 95.8522 16.8281 96.1816 17.1719C96.5182 17.5085 96.7725 17.9417 96.9443 18.4717C97.1162 19.0016 97.2021 19.6318 97.2021 20.3623V28H95.2041V20.3408C95.2041 19.6891 95.1003 19.1842 94.8926 18.8262C94.6921 18.4609 94.4056 18.2067 94.0332 18.0635C93.668 17.9131 93.2311 17.8379 92.7227 17.8379C92.2858 17.8379 91.8991 17.9131 91.5625 18.0635C91.2259 18.2139 90.943 18.4215 90.7139 18.6865C90.4847 18.9443 90.3092 19.2415 90.1875 19.5781C90.0729 19.9147 90.0156 20.2728 90.0156 20.6523ZM107.321 25.3145V16.377H109.319V28H107.418L107.321 25.3145ZM107.697 22.8652L108.524 22.8438C108.524 23.6172 108.442 24.3333 108.277 24.9922C108.12 25.6439 107.862 26.2096 107.504 26.6895C107.146 27.1693 106.677 27.5452 106.097 27.8174C105.517 28.0824 104.811 28.2148 103.98 28.2148C103.415 28.2148 102.896 28.1325 102.423 27.9678C101.957 27.8031 101.556 27.5488 101.22 27.2051C100.883 26.8613 100.622 26.4137 100.436 25.8623C100.257 25.3109 100.167 24.6484 100.167 23.875V16.377H102.154V23.8965C102.154 24.4193 102.212 24.8525 102.326 25.1963C102.448 25.5329 102.609 25.8014 102.81 26.002C103.017 26.1953 103.246 26.3314 103.497 26.4102C103.755 26.4889 104.02 26.5283 104.292 26.5283C105.137 26.5283 105.807 26.3672 106.301 26.0449C106.795 25.7155 107.149 25.2751 107.364 24.7236C107.586 24.165 107.697 23.5456 107.697 22.8652ZM114.336 18.8584V28H112.349V16.377H114.229L114.336 18.8584ZM113.863 21.748L113.036 21.7158C113.043 20.9209 113.161 20.1868 113.391 19.5137C113.62 18.8333 113.942 18.2425 114.357 17.7412C114.773 17.2399 115.267 16.8532 115.84 16.5811C116.42 16.3018 117.061 16.1621 117.763 16.1621C118.336 16.1621 118.851 16.2409 119.31 16.3984C119.768 16.5488 120.158 16.7923 120.48 17.1289C120.81 17.4655 121.061 17.9023 121.232 18.4395C121.404 18.9694 121.49 19.6175 121.49 20.3838V28H119.492V20.3623C119.492 19.7536 119.403 19.2666 119.224 18.9014C119.045 18.529 118.783 18.2604 118.439 18.0957C118.096 17.9238 117.673 17.8379 117.172 17.8379C116.678 17.8379 116.227 17.9417 115.818 18.1494C115.417 18.3571 115.07 18.6436 114.776 19.0088C114.49 19.374 114.264 19.793 114.1 20.2656C113.942 20.7311 113.863 21.2253 113.863 21.748ZM126.668 16.377V28H124.67V16.377H126.668ZM124.52 13.2939C124.52 12.9717 124.616 12.6995 124.81 12.4775C125.01 12.2555 125.304 12.1445 125.69 12.1445C126.07 12.1445 126.36 12.2555 126.561 12.4775C126.768 12.6995 126.872 12.9717 126.872 13.2939C126.872 13.6019 126.768 13.8669 126.561 14.0889C126.36 14.3037 126.07 14.4111 125.69 14.4111C125.304 14.4111 125.01 14.3037 124.81 14.0889C124.616 13.8669 124.52 13.6019 124.52 13.2939ZM134.725 16.377V17.9023H128.44V16.377H134.725ZM130.567 13.5518H132.555V25.1211C132.555 25.515 132.616 25.8122 132.737 26.0127C132.859 26.2132 133.017 26.3457 133.21 26.4102C133.403 26.4746 133.611 26.5068 133.833 26.5068C133.998 26.5068 134.17 26.4925 134.349 26.4639C134.535 26.4281 134.674 26.3994 134.768 26.3779L134.778 28C134.621 28.0501 134.413 28.0967 134.155 28.1396C133.905 28.1898 133.6 28.2148 133.242 28.2148C132.755 28.2148 132.308 28.1182 131.899 27.9248C131.491 27.7314 131.165 27.4092 130.922 26.958C130.686 26.4997 130.567 25.8838 130.567 25.1104V13.5518ZM140.321 26.7969L143.555 16.377H145.682L141.02 29.7939C140.912 30.0804 140.769 30.3883 140.59 30.7178C140.418 31.0544 140.196 31.373 139.924 31.6738C139.652 31.9746 139.322 32.2181 138.936 32.4043C138.556 32.5977 138.101 32.6943 137.571 32.6943C137.414 32.6943 137.213 32.6729 136.97 32.6299C136.726 32.5869 136.554 32.5511 136.454 32.5225L136.443 30.9111C136.501 30.9183 136.59 30.9255 136.712 30.9326C136.841 30.9469 136.93 30.9541 136.98 30.9541C137.432 30.9541 137.815 30.8932 138.13 30.7715C138.445 30.6569 138.71 30.46 138.925 30.1807C139.147 29.9085 139.337 29.5326 139.494 29.0527L140.321 26.7969ZM137.947 16.377L140.966 25.4004L141.481 27.4951L140.053 28.2256L135.777 16.377H137.947ZM155.457 12.3594V28H153.384V12.3594H155.457ZM160.914 18.8584V28H158.927V16.377H160.807L160.914 18.8584ZM160.441 21.748L159.614 21.7158C159.621 20.9209 159.74 20.1868 159.969 19.5137C160.198 18.8333 160.52 18.2425 160.936 17.7412C161.351 17.2399 161.845 16.8532 162.418 16.5811C162.998 16.3018 163.639 16.1621 164.341 16.1621C164.914 16.1621 165.429 16.2409 165.888 16.3984C166.346 16.5488 166.736 16.7923 167.059 17.1289C167.388 17.4655 167.639 17.9023 167.811 18.4395C167.982 18.9694 168.068 19.6175 168.068 20.3838V28H166.07V20.3623C166.07 19.7536 165.981 19.2666 165.802 18.9014C165.623 18.529 165.361 18.2604 165.018 18.0957C164.674 17.9238 164.251 17.8379 163.75 17.8379C163.256 17.8379 162.805 17.9417 162.396 18.1494C161.995 18.3571 161.648 18.6436 161.354 19.0088C161.068 19.374 160.842 19.793 160.678 20.2656C160.52 20.7311 160.441 21.2253 160.441 21.748ZM174.052 28H172.064V15.1523C172.064 14.3145 172.215 13.609 172.516 13.0361C172.824 12.4561 173.264 12.0192 173.837 11.7256C174.41 11.4248 175.09 11.2744 175.878 11.2744C176.107 11.2744 176.336 11.2887 176.565 11.3174C176.802 11.346 177.031 11.389 177.253 11.4463L177.146 13.0684C176.995 13.0326 176.823 13.0075 176.63 12.9932C176.444 12.9788 176.257 12.9717 176.071 12.9717C175.649 12.9717 175.284 13.0576 174.976 13.2295C174.675 13.3942 174.446 13.6377 174.288 13.96C174.131 14.2822 174.052 14.6797 174.052 15.1523V28ZM176.522 16.377V17.9023H170.228V16.377H176.522ZM178.209 22.3174V22.0703C178.209 21.2324 178.331 20.4554 178.574 19.7393C178.818 19.016 179.169 18.3893 179.627 17.8594C180.085 17.3223 180.64 16.9069 181.292 16.6133C181.944 16.3125 182.674 16.1621 183.483 16.1621C184.3 16.1621 185.034 16.3125 185.686 16.6133C186.344 16.9069 186.903 17.3223 187.361 17.8594C187.827 18.3893 188.181 19.016 188.425 19.7393C188.668 20.4554 188.79 21.2324 188.79 22.0703V22.3174C188.79 23.1553 188.668 23.9323 188.425 24.6484C188.181 25.3646 187.827 25.9912 187.361 26.5283C186.903 27.0583 186.348 27.4736 185.696 27.7744C185.052 28.068 184.321 28.2148 183.505 28.2148C182.688 28.2148 181.954 28.068 181.303 27.7744C180.651 27.4736 180.092 27.0583 179.627 26.5283C179.169 25.9912 178.818 25.3646 178.574 24.6484C178.331 23.9323 178.209 23.1553 178.209 22.3174ZM180.196 22.0703V22.3174C180.196 22.8975 180.264 23.4453 180.4 23.9609C180.536 24.4694 180.741 24.9206 181.013 25.3145C181.292 25.7083 181.639 26.0199 182.055 26.249C182.47 26.471 182.953 26.582 183.505 26.582C184.049 26.582 184.525 26.471 184.934 26.249C185.349 26.0199 185.693 25.7083 185.965 25.3145C186.237 24.9206 186.441 24.4694 186.577 23.9609C186.72 23.4453 186.792 22.8975 186.792 22.3174V22.0703C186.792 21.4974 186.72 20.9567 186.577 20.4482C186.441 19.9326 186.233 19.4779 185.954 19.084C185.682 18.6829 185.338 18.3678 184.923 18.1387C184.515 17.9095 184.035 17.7949 183.483 17.7949C182.939 17.7949 182.459 17.9095 182.044 18.1387C181.636 18.3678 181.292 18.6829 181.013 19.084C180.741 19.4779 180.536 19.9326 180.4 20.4482C180.264 20.9567 180.196 21.4974 180.196 22.0703Z" fill="#FAF9F5"/>
+<path d="M206.562 11.25H200.875C199.978 11.25 199.25 11.9775 199.25 12.875V29.125C199.25 30.0225 199.978 30.75 200.875 30.75H217.125C218.022 30.75 218.75 30.0225 218.75 29.125V23.4375C218.75 22.9888 218.386 22.625 217.938 22.625C217.489 22.625 217.125 22.9888 217.125 23.4375V29.125H200.875L200.875 12.875H206.562C207.011 12.875 207.375 12.5112 207.375 12.0625C207.375 11.6138 207.011 11.25 206.562 11.25Z" fill="white"/>
+<path d="M218.75 18.5625C218.75 19.0112 218.386 19.375 217.938 19.375C217.489 19.375 217.125 19.0112 217.125 18.5625V14.0241L206.083 25.0658L204.934 23.9167L215.976 12.875H211.438C210.989 12.875 210.625 12.5112 210.625 12.0625C210.625 11.6138 210.989 11.25 211.438 11.25H217.125C218.022 11.25 218.75 11.9775 218.75 12.875V18.5625Z" fill="white"/>
+</svg>

resources/docs/dimmable.svg
@@ -1,0 +1,14 @@
+<svg width="336" height="40" viewBox="0 0 336 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="1" y="1" width="334" height="38" rx="5" fill="#BBCEE0" stroke="#387DC5" stroke-width="2"/>
+<g clip-path="url(#clip0_15790_236175)">
+<rect x="255" width="40" height="336" transform="rotate(90 255 0)" fill="#387DC5"/>
+<rect x="247" y="6" width="4" height="28" rx="2" fill="white"/>
+</g>
+<path d="M50 33.125C50 33.6082 49.5523 34 49 34C48.4477 34 48 33.6082 48 33.125L48 6.875C48 6.39175 48.4477 6 49 6C49.5523 6 50 6.39175 50 6.875L50 33.125Z" fill="white" fill-opacity="0.3"/>
+<path d="M24.667 20.1973V21.0059C24.667 21.9668 24.5469 22.8281 24.3066 23.5898C24.0664 24.3516 23.7207 24.999 23.2695 25.5322C22.8184 26.0654 22.2764 26.4727 21.6436 26.7539C21.0166 27.0352 20.3135 27.1758 19.5342 27.1758C18.7783 27.1758 18.084 27.0352 17.4512 26.7539C16.8242 26.4727 16.2793 26.0654 15.8164 25.5322C15.3594 24.999 15.0049 24.3516 14.7529 23.5898C14.501 22.8281 14.375 21.9668 14.375 21.0059V20.1973C14.375 19.2363 14.498 18.3779 14.7441 17.6221C14.9961 16.8604 15.3506 16.2129 15.8076 15.6797C16.2646 15.1406 16.8066 14.7305 17.4336 14.4492C18.0664 14.168 18.7607 14.0273 19.5166 14.0273C20.2959 14.0273 20.999 14.168 21.626 14.4492C22.2588 14.7305 22.8008 15.1406 23.252 15.6797C23.709 16.2129 24.0576 16.8604 24.2979 17.6221C24.5439 18.3779 24.667 19.2363 24.667 20.1973ZM22.9883 21.0059V20.1797C22.9883 19.418 22.9092 18.7441 22.751 18.1582C22.5986 17.5723 22.373 17.0801 22.0742 16.6816C21.7754 16.2832 21.4092 15.9814 20.9756 15.7764C20.5479 15.5713 20.0615 15.4688 19.5166 15.4688C18.9893 15.4688 18.5117 15.5713 18.084 15.7764C17.6621 15.9814 17.2988 16.2832 16.9941 16.6816C16.6953 17.0801 16.4639 17.5723 16.2998 18.1582C16.1357 18.7441 16.0537 19.418 16.0537 20.1797V21.0059C16.0537 21.7734 16.1357 22.4531 16.2998 23.0449C16.4639 23.6309 16.6982 24.126 17.0029 24.5303C17.3135 24.9287 17.6797 25.2305 18.1016 25.4355C18.5293 25.6406 19.0068 25.7432 19.5342 25.7432C20.085 25.7432 20.5742 25.6406 21.002 25.4355C21.4297 25.2305 21.79 24.9287 22.083 24.5303C22.3818 24.126 22.6074 23.6309 22.7598 23.0449C22.9121 22.4531 22.9883 21.7734 22.9883 21.0059ZM28.5869 19.5205V27H26.9609V17.4902H28.499L28.5869 19.5205ZM28.2002 21.8848L27.5234 21.8584C27.5293 21.208 27.626 20.6074 27.8135 20.0566C28.001 19.5 28.2646 19.0166 28.6045 18.6064C28.9443 18.1963 29.3486 17.8799 29.8174 17.6572C30.292 17.4287 30.8164 17.3145 31.3906 17.3145C31.8594 17.3145 32.2812 17.3789 32.6562 17.5078C33.0312 17.6309 33.3506 17.8301 33.6143 18.1055C33.8838 18.3809 34.0889 18.7383 34.2295 19.1777C34.3701 19.6113 34.4404 20.1416 34.4404 20.7686V27H32.8057V20.751C32.8057 20.2529 32.7324 19.8545 32.5859 19.5557C32.4395 19.251 32.2256 19.0312 31.9443 18.8965C31.6631 18.7559 31.3174 18.6855 30.9072 18.6855C30.5029 18.6855 30.1338 18.7705 29.7998 18.9404C29.4717 19.1104 29.1875 19.3447 28.9473 19.6436C28.7129 19.9424 28.5283 20.2852 28.3936 20.6719C28.2646 21.0527 28.2002 21.457 28.2002 21.8848Z" fill="#FAF9F5"/>
+<defs>
+<clipPath id="clip0_15790_236175">
+<rect width="336" height="40" rx="6" fill="white"/>
+</clipPath>
+</defs>
+</svg>

commit 416a034f6e4be3c469e7263dda5c5a4491fd3691

Fix/issue 224 relay control
Squashed commit of the following:

commit 1c59cb149e467a599417cfd01c7093ba90da7c92
Author: Dirk-Jan Faber <dfaber@victronenergy.com>
Date:   Mon Jan 12 15:27:12 2026 +0100

    Update relay tests for new communityTag structure

    Adapt tests to expect { communityTag, services } return value instead of
    plain array from getRelayServices().

    Also includes minor cosmetic changes (trailing whitespace removal) from linter.

commit 924a9d7ed05772981b42c1eea2522c8196deb5ea
Author: Dirk-Jan Faber <dfaber@victronenergy.com>
Date:   Mon Jan 12 15:24:41 2026 +0100

    Fix getRelayServices() to return communityTag structure (issue #224)

    Regression from 829026d which added communityTag wrapper to getNodeServices()
    but missed getRelayServices(), causing output-relay dropdown to be empty.

commit 5f967c39595736de12ec6f95794ac2a37c480fe8
Author: Chris Oloff <chris@uber5.com>
Date:   Fri Jan 9 10:06:25 2026 +0200

    run linter

commit b15db2469a229b44e579ee7cb0f62c3e7929aa3d
Author: Chris Oloff <chris@uber5.com>
Date:   Fri Jan 9 09:06:16 2026 +0200

    styling radio group, simplified and better, wip

commit 3f1d349c3a08fbfffcf2298a516bfcabd105a465
Author: Chris Oloff <chris@uber5.com>
Date:   Fri Jan 9 07:50:12 2026 +0200

    styling radio group, simplified but wrong, wip

commit cb5a5bdbdeca1977ca5f4d46ca9cb77a7fe7e7b2
Author: Chris Oloff <chris@uber5.com>
Date:   Fri Jan 9 07:29:34 2026 +0200

    virtual devices views: add resources functions after 'npm run build'

commit 6c57761581bc52ed1ed2ce1b603614412cfd4674
Author: Chris Oloff <chris@uber5.com>
Date:   Thu Jan 8 12:39:24 2026 +0200

    virtual devices views: fix increasing whitespace when switching device type, fix battery note width

commit f222cf72af70ea0bd995778ea702256f3c426dd0
Author: Chris Oloff <chris@uber5.com>
Date:   Mon Dec 22 16:23:09 2025 +0200

    virtual switch, add missing 'CustomName' property

commit dea47c3ee9d808f4ab46e98e5467ccbef249c7e0

src/services/victron-system.js
@@ -132,7 +132,7 @@ class SystemConfiguration {
 
     // Filter all paths that begin with '/Relay'.
     // Construct and return an array of relay nodes representing their settings.
-    return Object.entries(this.cache)
+    const services = Object.entries(this.cache)
       .reduce((acc, [service, pathObj]) => {
         const relayPaths = Object.keys(pathObj).filter(path => path.startsWith('/Relay'))
         if (relayPaths.length) {
@@ -143,6 +143,11 @@ class SystemConfiguration {
         }
         return acc
       }, [])
+
+    return {
+      communityTag: _.get(utils.SERVICES, ['relay', 'communityTag']) || null,
+      services
+    }
   }
 
   getCachedServices () {

test/relay-expansion.test.js
@@ -38,14 +38,16 @@ describe('SystemConfiguration.getRelayServices wildcard expansion (PR #256)', ()
   test('getRelayServices returns relay services when paths exist in cache', () => {
     const result = systemConfig.getRelayServices()
 
-    expect(Array.isArray(result)).toBe(true)
-    
+    expect(result).toHaveProperty('communityTag')
+    expect(result).toHaveProperty('services')
+    expect(Array.isArray(result.services)).toBe(true)
+
     // Should find system service with relay paths
-    if (result.length > 0) {
-      const systemService = result.find(service => 
+    if (result.services.length > 0) {
+      const systemService = result.services.find(service =>
         service.service === 'com.victronenergy.system/0'
       )
-      
+
       if (systemService) {
         expect(systemService.name).toBe('Venus device')
         expect(systemService.paths).toBeDefined()
@@ -62,16 +64,18 @@ describe('SystemConfiguration.getRelayServices wildcard expansion (PR #256)', ()
         system: [] // Empty array - no relay definitions
       }
     }
-    
+
     // Should not throw error even with missing relay definitions (this is what PR #256 fixes)
     expect(() => {
       const result = systemConfig.getRelayServices()
-      expect(Array.isArray(result)).toBe(true)
+      expect(result).toHaveProperty('services')
+      expect(Array.isArray(result.services)).toBe(true)
     }).not.toThrow()
-    
+
     // The function should complete without crashing (core fix for issue #253)
     const result = systemConfig.getRelayServices()
-    expect(Array.isArray(result)).toBe(true)
+    expect(result).toHaveProperty('services')
+    expect(Array.isArray(result.services)).toBe(true)
   })
 
   test('getRelayServices filters out null relay objects', () => {
@@ -91,32 +95,35 @@ describe('SystemConfiguration.getRelayServices wildcard expansion (PR #256)', ()
         ]
       }
     }
-    
+
     const result = systemConfig.getRelayServices()
-    
+
     // Should handle null objects gracefully
     expect(() => {
       const result = systemConfig.getRelayServices()
-      expect(Array.isArray(result)).toBe(true)
+      expect(result).toHaveProperty('services')
+      expect(Array.isArray(result.services)).toBe(true)
     }).not.toThrow()
   })
 
   test('getRelayServices handles empty cache gracefully', () => {
     systemConfig.cache = {}
-    
+
     const result = systemConfig.getRelayServices()
-    
-    expect(Array.isArray(result)).toBe(true)
-    expect(result.length).toBe(0)
+
+    expect(result).toHaveProperty('services')
+    expect(Array.isArray(result.services)).toBe(true)
+    expect(result.services.length).toBe(0)
   })
 
   test('getRelayServices handles system relay function check', () => {
     // Test the system relay function logic (manual vs non-manual)
     systemConfig.cache['com.victronenergy.settings']['/Settings/Relay/Function'] = 1 // non-manual
-    
+
     expect(() => {
       const result = systemConfig.getRelayServices()
-      expect(Array.isArray(result)).toBe(true)
+      expect(result).toHaveProperty('services')
+      expect(Array.isArray(result.services)).toBe(true)
     }).not.toThrow()
   })
 
@@ -128,7 +135,7 @@ describe('SystemConfiguration.getRelayServices wildcard expansion (PR #256)', ()
         system: [
           {
             path: '/Relay/{relay}/State',
-            type: 'enum', 
+            type: 'enum',
             name: 'Venus relay {relay} state',
             enum: { '0': 'Open', '1': 'Closed' },
             mode: 'both'
@@ -136,11 +143,12 @@ describe('SystemConfiguration.getRelayServices wildcard expansion (PR #256)', ()
         ]
       }
     }
-    
+
     // Test that wildcard expansion doesn't break the function
     expect(() => {
       const result = systemConfig.getRelayServices()
-      expect(Array.isArray(result)).toBe(true)
+      expect(result).toHaveProperty('services')
+      expect(Array.isArray(result.services)).toBe(true)
     }).not.toThrow()
   })
 })