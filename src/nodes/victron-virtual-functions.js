9d53e205af7ba0a3a95faa740be849971462fd09
Dirk-Jan Faber <dfaber@victronenergy.com>
2026-01-18 12:10:26 +0100

    Improve readiblity for documentation check
    Via https://github.com/victronenergy/node-red-contrib-victron/pull/362#pullrequestreview-3665832473

--- src/nodes/victron-virtual-functions.js
+++ src/nodes/victron-virtual-functions.js
@@ -829,8 +829,8 @@ export function checkSelectedVirtualDevice (context) {
     $('#switch-docs-container').empty()
 
     // Show device-specific documentation if available
-    if (DEVICE_TYPE_DOCS[selected]) {
-      const doc = DEVICE_TYPE_DOCS[selected]
+    const doc = DEVICE_TYPE_DOCS[selected]
+    if (doc) {
       const docRow = $(`
         <div class="form-row">
           <div id="device-doc-row" class="victron-doc-box">
