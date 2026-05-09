90ffb391628b01f18a8cee447f0d45c64cb33cd4
Chris Oloff <chris@uber5.com>
2026-05-09 22:45:08 +0200

    revert changes to husky and lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,7 @@ module.exports = {
   '*.{js,jsx,ts,tsx}': [
     () => 'npm run build',
     () => 'npm run build:test',
+    'standard --fix',
     () => 'npm test'
   ],
   'src/nodes/*.html': [() => 'npm run documentation']
